6d85e828f6e382d4ab8b79e8aaf53f7dd4cfc0d0
AnastasiiaSyplyvets <flizz.sia@gmail.com>
2023-03-15 20:11:17 +0100

    a

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const startBtnEl = document.querySelector("button[data-start]");
+// console.log(startBtnEl)
+
+const stopBtnEl = document.querySelector("button[data-stop]");
+// console.log(stopBtnEl)
+
+const bodyEl = document.querySelector('body');
+// console.log(bodyEl)
+
+let timerId = null;
+
+startBtnEl.addEventListener('click', (event)=> {
+     timerId = setInterval(()=>{
+        bodyEl.style.backgroundColor = getRandomHexColor();
+            }, 1000);
+            startBtnEl.disabled = true;  
+            stopBtnEl.disabled = false;  
+})
+
+stopBtnEl.addEventListener('click', (event)=>{
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
